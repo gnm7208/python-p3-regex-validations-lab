226b58200685c845edb5c4da270f1c59ee645bb6
George <gnm7208@gmail.com>
2025-12-10 15:30:14 +0300

    Implement phone number regex pattern

--- lib/regex.py
+++ lib/regex.py
@@ -6,7 +6,7 @@ import re
 name = r"[A-Z][a-z]*'?[A-Za-z]*([- ][A-Z][a-z]*'?[A-Za-z]*)*"
 name_regex = re.compile(name)
 
-phone_number = r""
+phone_number = r"(\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})"
 phone_regex = re.compile(phone_number)
 
 email_address = r""
