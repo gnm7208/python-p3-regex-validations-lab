42280bcdaabb1aa2138ac14f933dc7766456237d
George <gnm7208@gmail.com>
2025-12-10 15:30:32 +0300

    Implement email regex pattern

--- lib/regex.py
+++ lib/regex.py
@@ -9,5 +9,5 @@ name_regex = re.compile(name)
 phone_number = r"(\d{10}|\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+email_address = r"[a-zA-Z][a-zA-Z0-9.]*@[a-zA-Z0-9]+\.[a-zA-Z]+"
 email_regex = re.compile(email_address)
